a9c61f9550eceef24737b2e0129dcd35159d8924
MillerBarrera <mbarrerag@unal.edu.co>
2024-01-16 15:54:13 -0500

    ResponseEntity, APIrest best practices

--- src/main/java/com/alura/foro/controllers/NewTopicController.java
+++ src/main/java/com/alura/foro/controllers/NewTopicController.java
@@ -3,6 +3,7 @@ package com.alura.foro.controllers;
 
 import com.alura.foro.records.DataNewTopic;
 import com.alura.foro.records.GetTopicRepository;
+import com.alura.foro.records.UpdateAnswerDataTopic;
 import com.alura.foro.records.UpdateTopic;
 import com.alura.foro.repository.CurseRepository;
 import com.alura.foro.repository.UserRepository;
@@ -17,8 +18,11 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.data.web.PageableDefault;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
 
+import java.net.URI;
 import java.util.Optional;
 
 @RestController
@@ -40,14 +44,21 @@ public class NewTopicController {
     }
 
     @PostMapping
-    public void newTopic(@RequestBody @Valid DataNewTopic dataNewTopic) {
+    public ResponseEntity<UpdateAnswerDataTopic> newTopic(@RequestBody @Valid DataNewTopic dataNewTopic, UriComponentsBuilder uriComponentsBuilder) {
         Usuario usuario = userRepository.getById(dataNewTopic.author());
         Curso curso = curseRepository.getById(dataNewTopic.curse());
 
         if ((usuario != null && curso != null)) {
-            topicoRepository.save(new Topico(dataNewTopic, usuario, curso));
+            Topico topico = topicoRepository.save(new Topico(dataNewTopic, usuario, curso));
+            UpdateAnswerDataTopic updateAnswerDataTopic = new UpdateAnswerDataTopic(topico.getId(), topico.getTitulo(), topico.getMensaje(), topico.getAutor().getId(), topico.getCurso().getId());
+
+//
+//            URI url = URI.create("http://localhost:8080/newtopics/all/" + topico.getId());
+            URI url = uriComponentsBuilder.path("/newtopics/all/{id}").buildAndExpand(topico.getId()).toUri();
+            return ResponseEntity.created(url).body(updateAnswerDataTopic);
         } else {
             System.out.println("usuario no encontrado");
+            return ResponseEntity.notFound().build();
         }
     }
 
@@ -56,43 +67,57 @@ public class NewTopicController {
         /* Form to make a query implicit into the Repository*/
         return topicoRepository.findByActiveTrue(pageable).map(GetTopicRepository::new);
     }
-//    @GetMapping("/all/{id}")
-//    public Optional<GetTopicRepository> getTopics(@PathVariable Long id){
-//        return topicoRepository.findAll().stream().filter(topico -> topico.getId().equals(id)).map(GetTopicRepository::new).findFirst();
-//    }
+
+    @GetMapping("/all/{id}")
+    public ResponseEntity<UpdateAnswerDataTopic> getTopics(@PathVariable Long id){
+        Topico topico = topicoRepository.getById(id);
+        if(topico != null){
+            var datosTopico = new UpdateAnswerDataTopic(topico.getId(), topico.getTitulo(), topico.getMensaje(), topico.getAutor().getId(), topico.getCurso().getId());
+            return ResponseEntity.ok(datosTopico);
+        }else{
+            return ResponseEntity.notFound().build();
+        }
+    }
 
     @Transactional
     @PutMapping("/all/update")
-    public void updateTopics(@RequestBody @Valid UpdateTopic updateTopic) {
+    public ResponseEntity<UpdateAnswerDataTopic> updateTopics(@RequestBody @Valid UpdateTopic updateTopic) {
 
         Topico topico = topicoRepository.getReferenceById(updateTopic.id());
 
         Usuario usuario;
         Curso curso;
         if (updateTopic.author() != null && updateTopic.curse() != null) {
-           usuario = userRepository.getById(updateTopic.author());
+            usuario = userRepository.getById(updateTopic.author());
         } else {
-           usuario = null;
+            usuario = null;
         }
-        if(updateTopic.curse() != null){
-             curso = curseRepository.getById(updateTopic.curse());
+        if (updateTopic.curse() != null) {
+            curso = curseRepository.getById(updateTopic.curse());
         } else {
             curso = null;
         }
         System.out.println("update topic");
         topico.upDateData(updateTopic, usuario, curso);
+        /*Is not recommendable return the entity so we create a DTO and return his values*/
+        return ResponseEntity.ok(new UpdateAnswerDataTopic(topico.getId(), topico.getTitulo(), topico.getMensaje(), topico.getAutor().getId(), topico.getCurso().getId()));
     }
 
 
     /*Logical deleted*/
     @Transactional
-   @DeleteMapping("/all/deleted/{id}")
-    public void deletedTopic(@PathVariable Long id){
+    @DeleteMapping("/all/deleted/{id}")
+    public ResponseEntity<Topico> deletedTopic(@PathVariable Long id) {
         Topico topico = topicoRepository.getReferenceById(id);
         /*Logic deleted*/
-        topico.desactiveTopic();
-    }
+        if (topico.getActive() == true) {
+            topico.desactiveTopic();
+            return ResponseEntity.ok().build();
+        } else {
+            return ResponseEntity.notFound().build();
 
+        }
+    }
 //      Physical deleted
 //    @Transactional
 //    @DeleteMapping("/all/deleted")
@@ -100,5 +125,6 @@ public class NewTopicController {
 //        Topico topico = topicoRepository.getReferenceById(updateTopic.id());
 //        topicoRepository.delete(topico);
 //    }
+
 }
 
---
+++ src/main/java/com/alura/foro/records/UpdateAnswerDataTopic.java
@@ -0,0 +1,4 @@
+package com.alura.foro.records;
+
+public record UpdateAnswerDataTopic(Long id, String tittle, String menssage, Long author, Long curse) {
+}
